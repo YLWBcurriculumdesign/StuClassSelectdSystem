40c0c1eacc983576e96253a3bccb0e14d8910238
Yang-Li-Chen <2572958006@qq.com>
2020-06-09 18:22:22 +0800

    判断 不会跳转

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,10 @@
-//let db = require("../models/db")
+let db = require("../models/db")
 exports.showIndex = (req,res)=>{
     res.render("index")
 }
 exports.dologin = (req,res)=>{
    console.log(req.body)
-   //  db.toologin(req.body,function(info){
-   //      res.send(info)
-   //  })
+    db.toologin(req.body,function(info){
+        res.send(info)
+    })
 }
