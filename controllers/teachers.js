be14871e32feac7c73502bc721a1b8bf70ec5bc0
LiWei <1115095260@qq.com>
2020-06-11 00:13:26 +0800

    没啥子

--- controllers/teachers.js
+++ controllers/teachers.js
@@ -7,11 +7,7 @@ exports.doRegister = (req,res)=>{
     console.log(req.body)
     tdb.add(req.body,function(info){
         if (info==1){
-            var user={
-                username:req.body.username,
-                password:req.body.password
-            }
-            req.session.user = user;
+            
             res.send(200);
         }else {
             req.session.error = "用户名或密码不正确";
