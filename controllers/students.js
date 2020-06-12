e22394a61d9a8237d0e64ba16e7414d2e5ea86ec
BeyondAlan <2864148964@qq.com>
2020-06-12 11:51:43 +0800

    修改数据

--- controllers/students.js
+++ controllers/students.js
@@ -1,4 +1,20 @@
+
+let sdb = require("../models/sdb");
+
 exports.showselect=(req,res)=>{
     res.render("Select_course")
 };
 
+
+//渲染一个处理学生修改个人信息的逻辑
+exports.doupdate=(req,res)=>{
+    console.log(req.body)
+    sdb.UPDATA(req.body,function(info) {
+        res.send(info);
+    })
+}
+
+//渲染一个显示学生修改个人信息的页面
+exports.update=(req,res)=>{
+    res.render('update')
+}
--- models/sdb.js
+++ models/sdb.js
@@ -40,6 +40,33 @@ function add(data,callback) {
     });
     
     }
-    
-    
+
+
     exports.add = add;
+
+//学生修改个人信息
+function UPDATA(data,callback) {
+    var mysql      = require('mysql');
+    var connection = mysql.createConnection({
+        host     : '39.101.177.156',
+        user     : 'root',
+        password : '555500',
+        database : 'studentsclass'
+    });
+    connection.connect();
+    var updataSql = 'UPDATE studata SET StudentName = ?,StudentSex = ?,StudentAge = ?,college = ?,major = ? WHERE StudentID = ?';
+    var updataSqlParams = [data.stuname, data.stusex, data.stuage, data.college, data.major, data.stuid];
+
+    connection.query(updataSql,updataSqlParams,function (err, result) {
+        if(err){
+            var str = err.message;
+            callback("-1");
+            connection.end();
+        }
+        console.log(data);
+        callback("1");
+        connection.end();
+    });
+}
+
+exports.UPDATA = UPDATA;  // 导出
