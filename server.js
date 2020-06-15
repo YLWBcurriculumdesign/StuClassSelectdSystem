60fc9ca8ed2104a0124521b2a75dfea0218f9d8e
Yang-Li-Chen <2572958006@qq.com>
2020-06-15 12:21:01 +0800

    推选查找

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ app.use(session({
 }));
 
 
-let index = require("./controllers/index")
+let index = require("./controllers/index");
 //教师
-let teacher = require("./controllers/teachers")
-let student = require("./controllers/students")
-let admin = require("./controllers/administrator")
+let teacher = require("./controllers/teachers");
+let student = require("./controllers/students");
+let admin = require("./controllers/administrator");
 
 app.set("view engine","ejs");
 // 修改模板文件的后缀名为html
@@ -54,25 +54,25 @@ app.get("/Admin_student",admin.admin_student);
 app.get("/Admin_course",admin.admin_course);
  app.get("/forgetPsw",index.showForget);
 app.get("/Student_select_course",student.Student_select_course);
-app.get("/Student_desselect_course",student.Student_desselect_course)
-app.get("/Student_my_course",student.student_my_course)
-app.get("/myclass",student.myclass)
+app.get("/Student_desselect_course",student.Student_desselect_course);
+app.get("/Student_my_course",student.student_my_course);
+app.get("/myclass",student.myclass);
 app.get("/regT",teacher.showRegT);
 app.get("/regS",admin.showRegS);
 app.get("/regC",admin.showRegC);
 //修改个人信息
 // app.get("/update",student.update)
-app.get("/STUupdate",student.showstudata)
-app.post("/STUupdate",student.doupdate)
-app.get("/SUpassword",student.showstupwd)
-app.post("/SUpassword",student.doupdatepwd)
-app.get("/T_update_password",teacher.showteapwd)
-app.post("/T_update_password",teacher.doupdatepwd)
-app.get("/TEAupdate",teacher.showteadata)
-app.post("/TEAupdate",teacher.doupdate)
-app.post("/regT",teacher.doRegT)
-app.post("/regS",admin.doRegS)
-app.post("/regC",admin.doRegC)
+app.get("/STUupdate",student.showstudata);
+app.post("/STUupdate",student.doupdate);
+app.get("/SUpassword",student.showstupwd);
+app.post("/SUpassword",student.doupdatepwd);
+app.get("/T_update_password",teacher.showteapwd);
+app.post("/T_update_password",teacher.doupdatepwd);
+app.get("/TEAupdate",teacher.showteadata);
+app.post("/TEAupdate",teacher.doupdate);
+app.post("/regT",teacher.doRegT);
+app.post("/regS",admin.doRegS);
+app.post("/regC",admin.doRegC);
 app.get('/logout', function(req, res){
     req.session.user = null;
     req.session.error = null;
